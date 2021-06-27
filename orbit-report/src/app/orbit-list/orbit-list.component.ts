4b47886f5d4b5543aea2925e36d7e630f5732f7f
Melinda <codegirl78@gmail.com>
2021-06-27 09:40:42 -0500

    Added given code just before constructor in orbit-list.component.ts

--- orbit-report/src/app/orbit-list/orbit-list.component.ts
+++ orbit-report/src/app/orbit-list/orbit-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrbitListComponent implements OnInit {
 
+  @Input() satellites: Satellite[];
   constructor() { }
 
   ngOnInit() {
